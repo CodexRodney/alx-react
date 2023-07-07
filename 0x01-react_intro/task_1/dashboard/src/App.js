71407258c7ab9dbbf62cf350f2ee03763d657c7a
CodexRodney <rodney002mwanje@gmail.com>
2023-07-08 00:36:51 +0300

    Added: Directory for task 1

---
+++ 0x01-react_intro/task_1/dashboard/src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <body>
+      <header>
+        <div className="App-header">
+          <img src="logo.svg" alt="logo" height='500' width='500' />
+          <h1> School Dashboard</h1>
+        </div>
+      </header>
+        <div className="App-body">
+          <p> Login to access the full dashboard</p>
+        </div>
+      <footer>
+        <div className="App-footer">
+            <p> Copyright 2020 - holberton School </p>
+        </div>
+      </footer>
+    </body> 
+  );
+}
+
+export default App;
